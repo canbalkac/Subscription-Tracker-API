b117db983d1ba9f3fe3e20613cc6621cb626884b
CAN BALKAÇ <78991668+canbalkac@users.noreply.github.com>
2025-06-01 03:02:56 +0300

    Subscription Tracker API, route, model and error middleware implementation

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import { PORT } from './config/env.js';
+//ROUTES
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import bodyParser from "express";
+
+const app = express();
+
+app.use(express.json()); // This allows our app to handle json data sent in request
+app.use(express.urlencoded({ extended: false })); // This helps us to process the form data sent via Html forms in a simple format
+app.use(cookieParser()); // Reads cookies from incoming requests so our apps can store user data
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the subscription tracker api");
+})
+
+app.listen(3000, async () => {
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+    await connectToDatabase();
+});
+
+export default app;
---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+   name: {
+       type: String,
+       required: [true, 'User name is required'],
+       trim: true,
+       minlength: 2,
+       maxlength: 50,
+   },
+    email: {
+        type: String,
+        required: [true, 'User Email is required'],
+        unique: [true, 'Email is required'],
+        trim: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'Invalid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, 'User Password is required'],
+        minlength: [6, 'Password must be at least 6 characters'],
+    }
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
